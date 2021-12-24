1930ff8122e1fe19a83d7067be56395433f28809
Mohanaa92 <49679529+Mohanaa92@users.noreply.github.com>
2021-12-24 18:10:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World FIXED!" );
-        System.out.println( "Hello World FIXED01!" );
+        System.out.println( "Hello World RS-1!" );
         
     }
 }
